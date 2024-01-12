11909720d0a0be3c43a536a11b1b19fa26b3caf7
Anshika Soni <anshikasoni357@gmail.com>
2024-01-12 22:56:06 +0530

    New commit

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -6,6 +6,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for positions in STARTING_POSITIONS:
@@ -20,4 +21,17 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.segments[0].setheading(90)
+
+    def down(self):
+        self.head.segments[0].setheading(270)
+
+    def left(self):
+        self.head.segments[0].setheading(180)
+
+    def right(self):
+        self.head.segments[0].setheading(0)
+
