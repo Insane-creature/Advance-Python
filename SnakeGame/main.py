a50a21ca2fd4c911856afef739e6473f0aac4f33
Anshika Soni <anshikasoni357@gmail.com>
2024-01-12 18:28:40 +0530

    Update

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -6,10 +6,19 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 starting_positions = [(0,0),(-20,0),(-40,0)]
+
+segments = []
+
 for positions in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
     new_segment.goto(positions)
+    segments.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 
 
