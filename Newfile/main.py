43200aca19e9f19d46a8440696dbfa2cbe449546
Anshika Soni <anshikasoni357@gmail.com>
2024-02-25 23:46:17 +0530

    Learning

--- Newfile/main.py
+++ Newfile/main.py
@@ -2,5 +2,14 @@
 #     contents = file.read()
 #     print(contents)
 
+# Overrites everything
 with open("my_file.txt", mode="w") as file:
     file.write("New text.")
+
+# Append the file
+with open("my_file.txt", mode="a") as file:
+    file.write("\nNew text.")
+
+# Creating new file
+with open("new_file.txt", mode="w") as file:
+    file.write("Creating through python, is fun.")
