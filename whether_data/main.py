57f128a0639e358e5660433f29631497dd213dc4
Anshika Soni <anshikasoni357@gmail.com>
2024-02-29 04:52:59 +0530

    New commit2

--- whether_data/main.py
+++ whether_data/main.py
@@ -4,5 +4,12 @@
 import csv
 
 with open("weather_data.csv", mode="r") as data:
-    print(csv.reader(data))
-
+    data_file = csv.reader(data)
+    temperatures = []
+    data.read
+    for row in data_file:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+        # temperatures.append(row[])
+    print(temperatures)
+    print(type(temperatures))
