81bd06b5469ccb7e38633fe062139d9cb7178eac
Anshika Soni <anshikasoni357@gmail.com>
2024-03-05 22:40:20 +0530

    Completed

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -15,6 +15,15 @@ guessed_state_list = []
 for i in range(len(state_names)):
     answer_state = screen.textinput(title=f"{len(guessed_state_list)}/50 State", prompt="What's another state's name?").title()
 
+    if answer_state == "Exit":
+        missing_states = []
+        
+        for i in state_names:
+            if i not in guessed_state_list:
+                missing_states.append(i)
+        new_data_to_learn = pandas.DataFrame(missing_states)
+        new_data_to_learn.to_csv("States_to_learn_data4.csv")
+        break
     if answer_state in state_names:
         guessed_state_list.append(answer_state)
         t = turtle.Turtle()
@@ -23,7 +32,4 @@ for i in range(len(state_names)):
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(state_data.state.item())
-
-# write("Hello there!")
-
-screen.exitonclick()
+   
