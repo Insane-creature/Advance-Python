0918ab22127a13458e8d90755dbf0b241374e0fb
Anshika Soni <anshikasoni357@gmail.com>
2024-03-09 00:40:55 +0530

    Args

--- Day 27_Tkinter, GUI/main.py
+++ Day 27_Tkinter, GUI/main.py
@@ -3,4 +3,12 @@ import tkinter
 window = tkinter.Tk()
 window.title("Home")
 
+window.minsize(width=500, height=300)
+my_label = tkinter.Label(text="I'm a Label")
+my_label.pack(expand=1)
+
+
+
+
+
 window.mainloop()
