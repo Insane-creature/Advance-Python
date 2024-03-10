f4df46d42f020f37e9696f07debce931a23922bb
Anshika Soni <anshikasoni357@gmail.com>
2024-03-10 21:45:22 +0530

    Tkinter grid

--- Day 28_PomodoroGUI/main.py
+++ Day 28_PomodoroGUI/main.py
@@ -19,14 +19,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.minsize(width=500, height=400)
 window.config(padx=20, pady=20, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness= 0)
+canvas = Canvas(width=300, height=400, bg=YELLOW )#, highlightthickness= 0)
 tomato_img = PhotoImage(file= "tomato.png")
-canvas.create_image(100, 112, image = tomato_img)
-canvas.create_text(100,122,text="00:00", fill= "white" ,font=(FONT_NAME, 30, "bold"))
-canvas.pack()
+canvas.create_image(150, 200, image = tomato_img)
+canvas.create_text(150,215,text="00:00", fill= "white" ,font=(FONT_NAME, 30, "bold"))
+canvas.
+canvas.grid()
 
-
+timer = Label(text="Timer")
+timer.grid()
 
 window.mainloop()
