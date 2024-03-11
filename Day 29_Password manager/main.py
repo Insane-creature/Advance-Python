f865cd1a1f22ca0fd8b4912ae5912860f61b3d24
Anshika Soni <anshikasoni357@gmail.com>
2024-03-11 22:57:14 +0530

    using grid and columnspan

--- Day 29_Password manager/main.py
+++ Day 29_Password manager/main.py
@@ -13,9 +13,31 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100,100, image = logo_image)
-canvas.pack()
+canvas.grid(column= 1,row=0)
 
+website = Label(text="Website")     # 1,0
+website.grid(column= 0,row=1)
 
+textbox1 = Entry(width=35)
+textbox1.grid(column= 1,row=1,columnspan=2)
+
+emailuser = Label(text="Email/Username")
+emailuser.grid(column= 0,row=2)
+
+textbox2 = Entry(width=35)
+textbox2.grid(column= 1,row=2,columnspan=2)
+
+password = Label(text="Password")
+password.grid(column= 0,row=3)
+
+textbox3 = Entry(width=21)
+textbox3.grid(column=1 ,row=3)
+
+generatepassword = Label(text="Generate Password")
+generatepassword.grid(column= 2,row=3)
+
+add = Button(text="Add", width=36)
+add.grid(column= 1, row=4, columnspan=2)
 
 
 
