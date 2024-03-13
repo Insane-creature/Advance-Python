6410975e9de372947c9fce2bd4fb1d0fa624d6b9
Anshika Soni <anshikasoni357@gmail.com>
2024-03-13 23:25:48 +0530

    Key error

--- Day 30_Exception, error & JSON data/main.py
+++ Day 30_Exception, error & JSON data/main.py
@@ -1,2 +1,7 @@
-with open("my_file.txt") as file:
-    file.read()
+# FileNotFoundError
+# with open("my_file.txt") as file:
+#     file.read()
+
+# Key Error
+a_dict = {"key":"value"}
+value = a_dict["non_existential_key"]
