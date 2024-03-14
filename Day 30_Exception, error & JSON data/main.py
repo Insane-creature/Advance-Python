fb386e6d78578e4c193d60e86c85e42a5d4c0cc9
Anshika Soni <anshikasoni357@gmail.com>
2024-03-14 17:15:05 +0530

    Else and final

--- Day 30_Exception, error & JSON data/main.py
+++ Day 30_Exception, error & JSON data/main.py
@@ -15,7 +15,17 @@ text = "abc"
 # print(text)
 
 try:
-    file = open("my_file.txt") 
-except: 
-    print("There was an error")
-
+    file = open("create_file.txt") 
+    a_dict = {"key":"value"}
+    print(a_dict["ksfksjf"])
+except FileNotFoundError: 
+    file = open("create_file.txt","w")
+    print("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("The file is closed.")
