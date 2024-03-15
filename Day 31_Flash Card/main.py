5342755069672c0522bd9b11672e029bc479ebd3
Anshika Soni <anshikasoni357@gmail.com>
2024-03-15 14:38:48 +0530

    Flashy flashy

--- Day 31_Flash Card/main.py
+++ Day 31_Flash Card/main.py
@@ -3,7 +3,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flashy")
-window.config(height=400, width=400, padx=50, pady=50, bg=BACKGROUND_COLOR)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=400, height=400, bg=BACKGROUND_COLOR, highlightthickness= 0)
 
@@ -12,6 +12,6 @@ card_front = PhotoImage(file= "card_front.png")
 
 canvas.create_image(100, 112, image = card_front)
 # canvas.create_image(100, 112, image = tomato_img)
-
+canvas.pack()
 
 window.mainloop()
