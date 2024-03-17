1ed689cfcbee581dfdd3b24b6597e1aaaa09b75e
Anshika Soni <anshikasoni357@gmail.com>
2024-03-17 22:23:05 +0530

    Flashy3

--- Day 31_Flash Card/main.py
+++ Day 31_Flash Card/main.py
@@ -7,13 +7,14 @@ BACKGROUND_COLOR = "#B1DDC6"
 # french_text = Label(text="")
 
 df= pd.read_csv("data/french_words.csv")
-df_dict = df.to_dict(orient="records")
-print(df_dict)
+to_learn = df.to_dict(orient="records")
+# print(df_dict)
 
 def next_card():
-    current_card = random.choice(df_dict)
-    canvas.itemconfig(card_title, text = "French")
-    canvas.itemconfig(card_word, text = current_card["French"])
+    current_card = random.choice(to_learn)
+    print(current_card["French"])
+    # canvas.itemconfig(card_title, text = "French")
+    # canvas.itemconfig(card_word, text = current_card["French"])
 
 
 window = Tk()
@@ -23,8 +24,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness= 0)
 card_front = PhotoImage(file= "images/card_front.png")
 canvas.create_image(400, 263, image = card_front)
-card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="", font=("Ariel", 40, "bold"))
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="", font=("Ariel", 40, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column= 0,row= 0, columnspan=2)
 
@@ -32,11 +33,11 @@ canvas.grid(column= 0,row= 0, columnspan=2)
 # canvas.create_image(100, 112, image = card_back)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=next_card)
 right_button.grid(column= 0,row= 1)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column= 1,row= 1)
 
 window.mainloop()
