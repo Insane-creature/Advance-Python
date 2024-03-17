4661a54a1e61cd87d574536dce8f705b711b0f5d
Anshika Soni <anshikasoni357@gmail.com>
2024-03-17 23:32:07 +0530

    Sended first automated mail

--- Day 32_Sending mail smtplib/main.py
+++ Day 32_Sending mail smtplib/main.py
@@ -3,11 +3,15 @@ import smtplib
 my_email = "anshikasoni323@gmail.com"
 password = "znexlybeihcnsruv"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-# TLS: transport layer security, secures connection
-connection.starttls()
-connection.login(user= my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="anshikasoni357@gmail.com", msg="Hi there!")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    # TLS: transport layer security, secures connection
+    connection.starttls()
+    connection.login(user= my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email, 
+        to_addrs="anshikasoni357@gmail.com", 
+        msg="Subject:Hi there!\n\nThis is mail body"
+    )
+
 
 
