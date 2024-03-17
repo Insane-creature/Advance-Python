607c9ca069169379c9aaa9d3817408e62ee14b76
Anshika Soni <anshikasoni357@gmail.com>
2024-03-18 00:05:09 +0530

    Monday motivation on monday lol

--- Day 32_Sending mail smtplib/main.py
+++ Day 32_Sending mail smtplib/main.py
@@ -1,17 +1,22 @@
-import smtplib
+# import smtplib
 
-my_email = "anshikasoni323@gmail.com"
-password = "znexlybeihcnsruv"
+# my_email = "anshikasoni323@gmail.com"
+# password = "znexlybeihcnsruv"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    # TLS: transport layer security, secures connection
-    connection.starttls()
-    connection.login(user= my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email, 
-        to_addrs="anshikasoni357@gmail.com", 
-        msg="Subject:Hi there!\n\nThis is mail body"
-    )
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     # TLS: transport layer security, secures connection
+#     connection.starttls()
+#     connection.login(user= my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email, 
+#         to_addrs="anshikasoni357@gmail.com", 
+#         msg="Subject:Hi there!\n\nThis is mail body"
+#     )
 
+import datetime as dt
+# module importing as dt
 
+# print(dt.datetime.now()) # module.class - datetime is class of module dt
+now = dt.datetime.now()
+print(now.strftime("%A"))
 
