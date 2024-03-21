85def2867b794c4f7e7885e829a70bb366679867
Anshika Soni <anshikasoni357@gmail.com>
2024-03-21 23:22:12 +0530

    Added constructor

--- Day 34_API, Creating Quiz App/main.py
+++ Day 34_API, Creating Quiz App/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import quizinterface
 
 question_bank = []
 for question in question_data:
@@ -9,8 +10,10 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+# quiz = quizinterface(q)
 
 quiz = QuizBrain(question_bank)
+quiz_ui = quizinterface()
 
 while quiz.still_has_questions():
     quiz.next_question()
--- Day 34_API, Creating Quiz App/ui.py
+++ Day 34_API, Creating Quiz App/ui.py
@@ -1,2 +1,13 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class quizinterface():
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("My title")
+
+        self.window.mainloop()
+
+quizinterface()
