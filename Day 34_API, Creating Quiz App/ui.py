3531358212f3f70d21a248c77b8c47a2b666379b
Anshika Soni <anshikasoni357@gmail.com>
2024-03-24 22:24:12 +0530

    .

--- Day 34_API, Creating Quiz App/ui.py
+++ Day 34_API, Creating Quiz App/ui.py
@@ -14,7 +14,7 @@ class QuizInterface():
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150,125,text="Some Question Text: ", fill=THEME_COLOR, font=("Arial", 20, "italic"))
-        self.canvas.grid(row=1, column=0 ,columnspan=2)
+        self.canvas.grid(row=1, column=0 ,columnspan=2, pady=50)
 
         # self.canvas.create_image = PhotoImage("images/false.png")
         # self.
