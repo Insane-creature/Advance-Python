88a03d5e6285709b219a83805ecf8ca4ae96605d
Anshika Soni <anshikasoni357@gmail.com>
2024-03-26 22:28:02 +0530

    API key

--- Day 34_rain_alert/main.py
+++ Day 34_rain_alert/main.py
@@ -2,6 +2,7 @@ import requests
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_keys = "34cc646d93c72ae9fa0987a90321e0a4"
+
 parameters = {
     "lat": 23.022505,
     "long": 72.571365,
@@ -11,6 +12,6 @@ parameters = {
 response = requests.get(OWM_Endpoint, params=parameters)
 # response.raise_for_status()
 print(response.status_code)
-# data = response.json()
+print(response.json())
 # # question_data = data["results"]
 # print(data)
