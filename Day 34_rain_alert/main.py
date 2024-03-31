663698b4e09822df65daffea68f768fc7d54eed2
Anshika Soni <anshikasoni357@gmail.com>
2024-03-31 23:59:10 +0530

    Adding alert codes

--- Day 34_rain_alert/main.py
+++ Day 34_rain_alert/main.py
@@ -11,9 +11,15 @@ parameters = {
 }
 
 response = requests.get(OWM_Endpoint, params=parameters)
-# response.raise_for_status()
-print(response.status_code)
+response.raise_for_status()
 weather_data = response.json()
-print(weather_data)
-# # question_data = data["results"]
-# print(data)
+# print(weather_data["list"][0]["weather"]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_data = hour_data["weather"][0]["id"]
+    if int(condition_data) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
