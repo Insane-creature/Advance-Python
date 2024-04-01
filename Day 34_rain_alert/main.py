08b8d94f068825a3cfb4b6089dce0a0bbef82e91
Anshika Soni <anshikasoni357@gmail.com>
2024-04-01 23:52:28 +0530

    Stuck

--- Day 34_rain_alert/main.py
+++ Day 34_rain_alert/main.py
@@ -1,25 +1,25 @@
 import requests
 
-OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid={API key}"
 api_keys = "34cc646d93c72ae9fa0987a90321e0a4"
 
 parameters = {
-    "lat": 23.022505,
-    "long": 72.571365,
-    "appid": api_keys,
-    "cnt": 4
+    "lat": 51.507351,
+    "long": -0.127758,
+    "appid": "635ba07154facb3e484a55a532b44c33"
+    # "cnt": 4
 }
 
 response = requests.get(OWM_Endpoint, params=parameters)
-response.raise_for_status()
-weather_data = response.json()
+# response.raise_for_status()
+print(response.json())
 # print(weather_data["list"][0]["weather"]["id"])
 
-will_rain = False
-for hour_data in weather_data["list"]:
-    condition_data = hour_data["weather"][0]["id"]
-    if int(condition_data) < 700:
-        will_rain = True
+# will_rain = False
+# for hour_data in weather_data["list"]:
+#     condition_data = hour_data["weather"][0]["id"]
+#     if int(condition_data) < 700:
+#         will_rain = True
 
-if will_rain:
-    print("Bring an umbrella.")
+# if will_rain:
+#     print("Bring an umbrella.")
