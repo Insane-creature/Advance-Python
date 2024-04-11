09c871657717074b6d4bb670ed77ad39ee9d892a
Anshika Soni <anshikasoni357@gmail.com>
2024-04-11 23:51:41 +0530

    Adding parameters

--- Day 35_Stock Trading news alert/main.py
+++ Day 35_Stock Trading news alert/main.py
@@ -5,4 +5,7 @@ STOCK_START = "https://alphavantage.co/query"
 STOCK_END = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = 
-requests.get(STOCK_END)
+stock_paras = {
+    "function": "TIME_SERIES_DAILY"
+}
+requests.get(STOCK_END, stock_paras)
