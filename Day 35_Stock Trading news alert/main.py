f26294e373d6b612f334bca3a001da607f23ae9c
Anshika Soni <anshikasoni357@gmail.com>
2024-04-17 23:11:33 +0530

    Re-starting

--- Day 35_Stock Trading news alert/main.py
+++ Day 35_Stock Trading news alert/main.py
@@ -1,13 +1,18 @@
+import requests
+
 stock_name = "TSLA"
 company_name = "Tesla Inc"
 
-STOCK_START = "https://alphavantage.co/query"
-STOCK_END = "https://newsapi.org/v2/everything"
+STOCK_ENDPOINT = "https://alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-STOCK_API_KEY = ""
+STOCK_API_KEY = "2edb90f3b9214c2fa234da5f4ab67a79"
+
 alphavantage_api = ""
 
 stock_paras = {
-    "function": "TIME_SERIES_DAILY"
+    "function": "TIME_SERIES_DAILY",
+    "symbol": stock_name,
+    "apikey": STOCK_API_KEY
 }
-requests.get(STOCK_END, stock_paras)
+requests.get(STOCK_ENDPOINT, params)
