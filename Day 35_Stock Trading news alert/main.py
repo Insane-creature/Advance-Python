5dce9758c20766865c1bfc6a77ffcee52d785341
Anshika Soni <anshikasoni357@gmail.com>
2024-04-18 23:06:35 +0530

    Working

--- Day 35_Stock Trading news alert/main.py
+++ Day 35_Stock Trading news alert/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 stock_name = "TSLA"
 company_name = "Tesla Inc"
@@ -8,8 +9,8 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "2edb90f3b9214c2fa234da5f4ab67a79"
 news_api = "2edb90f3b9214c2fa234da5f4ab67a79"
-
-alphavantage_api = ""
+Twilio_sid = "ACef3fb8133fd356c9dc20e57abea2a19d"
+auth_token = "f2e58d05b1cea1b020cf9d8ca707186b"
 
 stock_paras = {
     "function": "TIME_SERIES_DAILY",
@@ -27,38 +28,50 @@ data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
-print(yesterday_closing_price)
+# print(yesterday_closing_price)
 
 # DAY BEFORE YESTERDAY'S STOCK PRICE
 daybefore_yesterday_data = data_list[1]
 daybefore_yesterday_closing_price = daybefore_yesterday_data["4. close"]
-print(daybefore_yesterday_closing_price)
-
 # print(daybefore_yesterday_closing_price)
-
-print(response.json())
+# print(response.json())
 
 # Increase/decrease in STOCK price
-difference = abs(float(yesterday_closing_price)-float(daybefore_yesterday_closing_price))
+difference = float(yesterday_closing_price)-float(daybefore_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
 # print(difference)
 average = (float(yesterday_closing_price)+float(daybefore_yesterday_closing_price))/2
-diff_percentage = (difference/average)*100
+diff_percentage = round(difference/average)*100
 print(diff_percentage)
 
-news_paras = {
-    "q": stock_name,
-    "apiKey": news_api
-}
+if abs(diff_percentage) >= 0:    
+    news_paras = {
+        "q": stock_name,
+        "apiKey": news_api
+    }
 
-news_response = requests.get(NEWS_ENDPOINT, news_paras)
-articles = news_response.json()["articles"]
-# print(articles)
-# data_list = [value for (key, value) in data.items()]
-# print(data_list)
+    news_response = requests.get(NEWS_ENDPOINT, news_paras)
+    articles = news_response.json()["articles"]
+    # print(articles)
+    # data_list = [value for (key, value) in data.items()]
+    # print(data_list)
 
-three_articles = articles[:3]
-# print(three_articles)
+    three_articles = articles[:3]
+    # print(three_articles)
 
-formatted_article = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    formatted_article = [f"{stock_name}: {up_down}{diff_percentage}%\n Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    # print(formatted_article)
 
-print(formatted_article)
+    client = Client(Twilio_sid, auth_token)
+    for article in formatted_article:
+        message = client.messages.create(
+            body=article,
+            from_="+12564881761",
+            to="+918074972210"
+
+        )
