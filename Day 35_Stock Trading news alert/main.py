bdd5d7d6eacc4fbd64e9736bf1ac6d9209835356
Anshika Soni <anshikasoni357@gmail.com>
2024-04-18 08:52:05 +0530

    Task 1 completed

--- Day 35_Stock Trading news alert/main.py
+++ Day 35_Stock Trading news alert/main.py
@@ -16,11 +16,30 @@ stock_paras = {
     "apikey": STOCK_API_KEY
 }
 response = requests.get(STOCK_ENDPOINT, stock_paras)
+
+# YESTERDAY'S STOCK PRICE
 # yesterday_data = response.json()["Time Series (Daily)"]["2024-04-16"]["1. open"]
-data = response.json()["Time Series (Daily)"]
-data_list = [value for (key, value) in data.items()]
-yesterday_data = data_list[0]
-yesterday_closing_price = yesterday_data["4. close"]
-print(yesterday_closing_price)
+data1 = response.json()
+print(data1)
+# data = response.json()["Time Series (Daily)"]
+# # print(data)
+# data_list = [value for (key, value) in data.items()]
+# yesterday_data = data_list[0]
+# yesterday_closing_price = yesterday_data["4. close"]
+# print(yesterday_closing_price)
+
+# # DAY BEFORE YESTERDAY'S STOCK PRICE
+# daybefore_yesterday_data = data_list[1]
+# daybefore_yesterday_closing_price = daybefore_yesterday_data["4. close"]
+# print(daybefore_yesterday_closing_price)
+
+# # print(daybefore_yesterday_closing_price)
 
 # print(response.json())
+
+# # Increase/decrease in STOCK price
+# difference = abs(float(yesterday_closing_price)-float(daybefore_yesterday_closing_price))
+# # print(difference)
+# average = (float(yesterday_closing_price)+float(daybefore_yesterday_closing_price))/2
+# diff_percentage = (difference/average)*100
+# print(diff_percentage)
