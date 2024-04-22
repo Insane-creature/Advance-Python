29d40679be04ab476cb3ef3ea4f4d53b2f3296a5
Anshika Soni <anshikasoni357@gmail.com>
2024-04-22 17:21:38 +0530

    POSTING

--- Day 36_API Habbit tracker/main.py
+++ Day 36_API Habbit tracker/main.py
@@ -1,13 +1,20 @@
 import requests
 
+USERNAME = "sonianshika"
+TOKEN = "ksdk23asjd345khks3l4"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "ksdk23asjd345khks3l4",
-    "username": "sonianshika",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
 response = requests.post(pixela_endpoint, json=user_params)
 print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+requests.post()
