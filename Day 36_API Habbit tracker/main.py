530c5d43ff21053a33262791941c26e08b8f1adb
Anshika Soni <anshikasoni357@gmail.com>
2024-04-23 22:38:25 +0530

    Post

--- Day 36_API Habbit tracker/main.py
+++ Day 36_API Habbit tracker/main.py
@@ -11,13 +11,21 @@ user_params = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-response = requests.post(pixela_endpoint, json=user_params)
-print(response.text)
-
+# response = requests.post(pixela_endpoint, json=user_params)
+# print(response.text)
 
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
-requests.post()
+graph_config = {
+    "id": "graph1",
+    "name": "Cylic graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config)
+print(response.text)
 
 
